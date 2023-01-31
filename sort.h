c339a16baf387c3ed9adb4ecaa2c8bc353de8103
wachaidickson@gmail.com <wachaidickson@gmail.com>
2023-01-30 22:59:44 -0800

    Correct checker error

--- sort.h
+++ sort.h
@@ -1,5 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
+void swap_ints(int *a, int *b);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
